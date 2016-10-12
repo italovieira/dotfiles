a9915e93bdb1adfd38f1c68bd1e374c34f3d37f1
Ítalo Vieira <italogv@hotmail.com>
2016-10-12 16:27:41 -0300

    Set up gpg-agent to cache SSH keys

---
+++ zsh/.zsh/gpg-agent.zsh
@@ -0,0 +1,12 @@
+# Start the gpg-agent if not already running
+if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
+  gpg-connect-agent /bye >/dev/null 2>&1
+fi
+
+# Set SSH to use gpg-agent
+unset SSH_AGENT_PID
+if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+fi
+
+export GPG_TTY=$(tty)
