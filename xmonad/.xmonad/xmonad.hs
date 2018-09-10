245bef8aea63ad7f2b53781dd06ba288dac7db8b
Ítalo Vieira <italogv@hotmail.com>
2018-09-09 22:41:30 -0300

    xmonad: Avoid windows from overlapping status bar
    https://github.com/xmonad/xmonad/issues/21#issuecomment-271209431
    http://hackage.haskell.org/package/xmonad-contrib-0.14/docs/XMonad-Config-Desktop.html#g:1

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,10 +1,11 @@
 import XMonad
 import XMonad.Util.Run (spawnPipe)
+import XMonad.Config.Desktop
 
 main = do
     spawnPipe "xmobar"
 
-    xmonad def
+    xmonad desktopConfig
         { modMask  = mod4Mask
         , terminal = "termite"
         }
