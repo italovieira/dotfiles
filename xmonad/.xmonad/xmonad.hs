a54594322106bc0f4f89535b4c96eb8abcb644f6
Ítalo Vieira <italogv@hotmail.com>
2019-07-01 18:06:55 -0300

    xmonad: Add Additional keys

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,5 +1,6 @@
 import XMonad
 import XMonad.Util.Run (spawnPipe, hPutStrLn)
+import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Config.Desktop
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Hooks.DynamicLog
@@ -7,10 +8,14 @@ import XMonad.Hooks.DynamicLog
 main = do
     h <- spawnPipe "xmobar"
 
-    xmonad desktopConfig
+    xmonad $ desktopConfig
         { modMask  = mod4Mask
         , terminal = "alacritty -e tmux"
         , layoutHook = smartBorders $ layoutHook desktopConfig
         , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn h }
-        , workspaces = map show [1..9]
-        }
+        } `additionalKeys`
+        [ ((mod4Mask, xK_F1),  spawn "bluetoothctl power on")
+        , ((mod4Mask, xK_F10), spawn "playerctl play-pause")
+        , ((mod4Mask, xK_F11), spawn "playerctl previos")
+        , ((mod4Mask, xK_F12), spawn "playerctl next")
+        ]
