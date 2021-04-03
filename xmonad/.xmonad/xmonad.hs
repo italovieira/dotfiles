fe4af9954d3162f789d1bd9f0840c9d5df1f3a11
Ítalo Vieira <italogomesv@gmail.com>
2021-04-02 22:42:37 -0300

    xmonad: Update keybindings

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -1,6 +1,6 @@
 import XMonad
 import XMonad.Util.Run (spawnPipe, hPutStrLn)
-import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Config.Desktop
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Hooks.DynamicLog
@@ -9,13 +9,20 @@ main = do
     spawn "~/.fehbg"
     h <- spawnPipe "xmobar"
 
+    let outputFile = " $HOME/Pictures/screenshots/$(date +%F-%T).png"
+    let notifyScreenshot = " && notify-send 'Screenshot saved to clipboard'"
+    let copyToClipboard = " | xclip -selection clipboard -t image/png -i"
+
     xmonad $ desktopConfig
         { modMask  = mod4Mask
         , terminal = "alacritty -e tmux"
         , layoutHook = smartBorders $ layoutHook desktopConfig
         , logHook = dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn h }
-        } `additionalKeys`
-        [ ((mod4Mask, xK_p),  spawn "rofi -show")
-        , ((mod4Mask, xK_F1), spawn "bluetoothctl power on")
-        , ((mod4Mask .|. shiftMask, xK_x),  spawn "loginctl lock-session")
+        } `additionalKeysP`
+        [ ("M-p", spawn "rofi -show")
+        , ("M-<F1>", spawn "bluetoothctl power on && notify-send 'Bluetooth turned on'")
+        , ("M-S-x",  spawn "loginctl lock-session")
+        , ("<Print>", spawn $ "maim " ++ copyToClipboard ++ notifyScreenshot)
+        , ("S-<Print>", spawn $ "maim -s " ++ copyToClipboard ++ notifyScreenshot)
+        , ("C-<Print>", spawn $ "maim -i $(xdotool getactivewindow) " ++ copyToClipboard ++ notifyScreenshot)
         ]
