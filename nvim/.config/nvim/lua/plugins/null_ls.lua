b0dc06916684796cbc97e398c9a5349c979b5142
Ítalo Vieira <italogomesv@gmail.com>
2022-10-09 11:05:24 -0300

    nvim: Add more sources for null-ls

--- nvim/.config/nvim/lua/plugins/null_ls.lua
+++ nvim/.config/nvim/lua/plugins/null_ls.lua
@@ -2,11 +2,14 @@ local  null_ls = require('null-ls')
 
 local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
+local code_actions = null_ls.builtins.code_actions
 
 null_ls.setup({
   sources = {
     formatting.autopep8,
     formatting.prettier,
+    formatting.jq,
     diagnostics.flake8,
+    code_actions.gitsigns,
   }
 })
