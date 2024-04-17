56ab4dff227181e7077f80373425227c3efbc317
Ítalo Vieira <italogomesv@gmail.com>
2024-04-17 00:03:02 -0300

    zsh: Set a better format for `time`

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -8,3 +8,6 @@ setopt HIST_EXPIRE_DUPS_FIRST
 setopt HIST_IGNORE_DUPS
 
 ZLE_SPACE_SUFFIX_CHARS=$'&|'
+
+# https://unix.stackexchange.com/a/453339.
+TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
