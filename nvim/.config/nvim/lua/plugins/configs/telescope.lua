8e56ef8fa348ae3b3e69ee34bfa2eab8c8f60eeb
Ítalo Vieira <italogomesv@gmail.com>
2024-12-22 01:02:15 -0300

    nvim: Fix telescope not loading fzf extension
    See https://youtu.be/xdXE1tOT-qg?t=17.

--- nvim/.config/nvim/lua/plugins/configs/telescope.lua
+++ nvim/.config/nvim/lua/plugins/configs/telescope.lua
@@ -1,6 +1,12 @@
+require('telescope').setup({
+  extensions = {
+    fzf = {}
+  }
+})
+
 require('telescope').load_extension('fzf')
 
-telescope = require('telescope.builtin')
+local telescope = require('telescope.builtin')
 
 vim.keymap.set('n', '<leader>ff', telescope.find_files)
 vim.keymap.set('n', '<leader>fg', telescope.live_grep)
