bb11b50331d8c5d1daf7f654286a070423bc6a6a
Ítalo Vieira <italogomesv@gmail.com>
2025-03-31 21:47:33 -0300

    nvim: Use virtual_lines on all lines and set map to toggle it

--- nvim/.config/nvim/lua/diagnostic.lua
+++ nvim/.config/nvim/lua/diagnostic.lua
@@ -1,9 +1,15 @@
 vim.diagnostic.config({
   -- https://github.com/neovim/neovim/pull/31959
-  virtual_lines = { current_line = true },
+  virtual_lines = true,
   signs = true,
   underline = false,
   update_in_insert = false,
 })
+
+-- :help diagnostic-toggle-virtual-lines-example
+vim.keymap.set('n', '<space>e', function()
+  local new_config = not vim.diagnostic.config().virtual_lines
+  vim.diagnostic.config({ virtual_lines = new_config })
+end, { desc = 'Toggle diagnostic virtual_lines' })
 
 vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
