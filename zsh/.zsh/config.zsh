d118581eecd72b7f70cf657aecb564810311840b
Ítalo Vieira <italogomesv@gmail.com>
2026-05-24 09:38:21 -0300

    zsh: Treat / as a word boundary
    This drops / (slash) from WORDCHARS so ZLE treats it as a word boundary.
    Word operations (Ctrl-W, Alt-F, Alt-B) then stop at each path component
    instead of jumping over the whole path.

--- zsh/.zsh/config.zsh
+++ zsh/.zsh/config.zsh
@@ -10,5 +10,8 @@ setopt HIST_IGNORE_DUPS
 
 ZLE_SPACE_SUFFIX_CHARS=$'&|'
 
+# Treat / as a word boundary
+WORDCHARS=${WORDCHARS//[\/]}
+
 # https://unix.stackexchange.com/a/453339.
 TIMEFMT=$'\n================\nCPU\t%P\nuser\t%*U\nsystem\t%*S\ntotal\t%*E'
